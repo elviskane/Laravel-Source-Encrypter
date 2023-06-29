ed5593aeedae3884a5158f4c3882a9868cdecce2
elviskane <44468763+elviskane@users.noreply.github.com>
2023-06-29 16:05:07 +0300

    Update SourceEncryptCommand.php

--- src/SourceEncryptCommand.php
+++ src/SourceEncryptCommand.php
@@ -132,9 +132,7 @@ class SourceEncryptCommand extends Command
 
         $fileContents = File::get(base_path($filePath));
 
-        $prepend = !is_null(env('PHP_BOLT_KEY')) ? "<?php
-bolt_decrypt( __FILE__ , env('PHP_BOLT_KEY')); return 0;
-##!!!##":"<?php
+        $prepend = "<?php
 bolt_decrypt( __FILE__ , '$key'); return 0;
 ##!!!##";
         $pattern = '/\<\?php/m';
